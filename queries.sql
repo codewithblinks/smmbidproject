0d55065cbee5b13f93e17f86918850bb635aeb8a
Your Name <you@example.com>
2024-10-10 02:03:01 +0100

    Update queries.sql with new changes

--- queries.sql
+++ queries.sql
@@ -213,8 +213,7 @@ CREATE TABLE IF NOT EXISTS challenge (
   challenge_complete BOOLEAN DEFAULT false
 );
 
-ALTER TABLE challenge DROP CONSTRAINT challenge_user_week_unique;
-
+ALTER TABLE challenge ADD CONSTRAINT challenge_user_week_unique UNIQUE (user_id, week_start);
 
 CREATE TABLE IF NOT EXISTS "session" (
   "sid" varchar NOT NULL COLLATE "default",
